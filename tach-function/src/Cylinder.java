0bcf60b849da6d700283bad3b8cdf70063b22e93
datquocngo <shaco14091999@gmail.com>
2018-11-08 08:33:11 +0700

    add

---
+++ tach-function/src/Cylinder.java
@@ -0,0 +1,14 @@
+public class Cylinder {
+    public static double getVolume(int radius, int height){
+        double volume = getPerimeter(radius) * height + 2 * getBaseArea(radius);
+        return volume;
+    }
+
+    public static double getPerimeter(int radius) {
+        return 2 * Math.PI  * radius;
+    }
+
+    public static double getBaseArea(int radius) {
+        return Math.PI * radius * radius;
+    }
+}
---
+++ tach-function/src/CylinderTest.java
@@ -0,0 +1,7 @@
+public class CylinderTest {
+    public static void main(String[] args) {
+        System.out.println("Area of a Circle with radius 5: "+Cylinder.getBaseArea(5));
+        System.out.println("Perimeter of a Circle with radius 5: "+Cylinder.getPerimeter(5));
+        System.out.println("Volume of a Circle with radius 5 and height 3: "+Cylinder.getVolume(5, 3));
+    }
+}
